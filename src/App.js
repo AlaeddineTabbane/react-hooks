55a9341f44237bc1377d11ee372c6cfb05b2a611
Alaeddine Tabbane <aladin206@gmail.com>
2022-05-14 17:40:18 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('')
+  const [text1, setText1] = useState({name:'ala'})
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'text 1',
+      done: false
+    },
+    {
+      id: 2,
+      text: 'text 2',
+      done: false
+    },
+  ])
+
+  const deleteTask = (id) => {
+    let list = [...tasks]
+    list = list.filter(task => task.id !== id)
+    // this.setState({ tasks: list })
+    setTasks(list)
+  }
+
+  const addTask = () => {
+    if (text){
+      let list = [...tasks]
+      const lastTask = list[list.length - 1]
+      const newId = lastTask.id + 1
+      list.push({
+        id: newId,
+        text,
+        done: false
+      })
+      // this.setState({ tasks: list, text:'' })
+      setTasks(list)
+      setText('')
+    }
+  }
+
+  useEffect(() => {
+    console.log('mount');
+    setText1(oldText => {
+      return {...oldText, name:'sami'}
+    })
+    const oldText = text1
+    setText({...oldText,name:'newText'})
+
+    return () => {
+      console.log('unmount');
+    };
+  },[text])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input onChange={(e) => setText(e.target.value)} value={text} />
+        <button type='button' onClick={addTask}>Add task</button>
+      </form>
+      {
+        tasks.map((item, index) => {
+          return (
+            <p key={index}>({item.id}) {item.text} <span onClick={() => deleteTask(item.id)}>X</span></p>
+          )
+        })
+      }
     </div>
   );
 }
